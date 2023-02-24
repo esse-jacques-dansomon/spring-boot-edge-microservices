ccfadfc7ed8e15efed227b9822c132331c824144
Esse Dansomon <essedansomon@gmail.com>
2023-02-24 08:24:19 +0000

    add test

---
+++ microservice-clientui/src/test/java/com/clientui/microserviceclientui/MicroserviceClientuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientui.microserviceclientui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceClientuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
