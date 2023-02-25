80caee5aa769689a9727f68cf6acdfc2487daba4
Esse Dansomon <essedansomon@gmail.com>
2023-02-25 05:40:24 +0000

    add @RefreshScope to ApplicationPropertiesConfiguration

--- microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
+++ microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
@@ -1,6 +1,7 @@
 package com.mproduits.configurations;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("mes-configs")
+@RefreshScope
 public class ApplicationPropertiesConfiguration {
 
     private int limitDeProduits;
