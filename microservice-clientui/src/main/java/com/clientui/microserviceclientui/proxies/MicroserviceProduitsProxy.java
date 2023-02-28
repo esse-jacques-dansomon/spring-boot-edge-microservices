501735718414ca0f8d093e50cfd2c4e58d55baba
Esse Dansomon <essedansomon@gmail.com>
2023-02-28 22:01:24 +0000

    add Custom Loader

--- microservice-clientui/src/main/java/com/clientui/microserviceclientui/proxies/MicroserviceProduitsProxy.java
+++ microservice-clientui/src/main/java/com/clientui/microserviceclientui/proxies/MicroserviceProduitsProxy.java
@@ -1,13 +1,14 @@
 package com.clientui.microserviceclientui.proxies;
 
 import com.clientui.microserviceclientui.beans.ProductBean;
+import com.clientui.microserviceclientui.configuration.CustomLoadBalancerConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "microservice-produits")
+@FeignClient(name = "microservice-produits", configuration = CustomLoadBalancerConfiguration.class)
 public interface MicroserviceProduitsProxy {
 
     @GetMapping(value = "/Produits")
